dcc581de93433ae0541c340e88b59c2e4acd36d5
frank038 <42263206+frank038@users.noreply.github.com>
2019-03-07 09:06:16 +0000

    Add files via upload

---
+++ qmfm/cfg.py
@@ -0,0 +1,27 @@
+### main program
+# thumbnailers: 0 no - 1 yes
+USE_THUMB = 1
+# icon cell width
+ITEM_WIDTH = 164
+# icon cell height
+ITEM_HEIGHT = 164
+# icon size
+ICON_SIZE = 96
+# other icons size: link and permissions
+ICON_SIZE2 = 48
+# space between items
+ITEM_SPACE = 25
+# use background colour in the listview widgets: 0 no - 1 yes
+USE_BACKGROUND_COLOUR = 1
+# listview background color: red, green, blue
+ORED = 210
+OGREEN = 210
+OBLUE = 210
+
+### needed by pythumb
+# border color of thumbnail
+BORDER_COLOR_R = 0
+BORDER_COLOR_G = 0
+BORDER_COLOR_B = 0
+# thumbnail images cache
+XDG_CACHE_LARGE = "sh_thumbnails/large/"
