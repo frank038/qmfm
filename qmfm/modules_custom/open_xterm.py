121337f64a19e686ec4783f5d4b96cb4b977146f
frank038 <42263206+frank038@users.noreply.github.com>
2019-03-17 16:36:55 +0000

    Update open_xterm.py

--- qmfm/modules_custom/open_xterm.py
+++ qmfm/modules_custom/open_xterm.py
@@ -12,19 +12,21 @@ def mmodule_name():
 
 # 1 : one item selected - 2 : more than one item selected - 3 : one or more items selected- 4 on background - 5 always
 # action type
-def mmodule_type():
-    return 4
+def mmodule_type(mainLView):
+    if shutil.which("xterm"):
+        return 4
+    else:
+        return 0
 
 
 class ModuleCustom():
     
     def __init__(self, mainLView):
         
-        if shutil.which("xterm"):
-            try:
-                path = mainLView.lvDir
-                comm = "cd '{}' && /bin/bash".format(path)
-                command = ['xterm', '-fa', 'Monospace', '-fs', '12', '-fg', 'white', '-bg', 'black', '-geometry', '70x18', '-e', comm]
-                subprocess.Popen(command)
-            except:
-                pass
+        try:
+            path = mainLView.lvDir
+            comm = "cd '{}' && /bin/bash".format(path)
+            command = ['xterm', '-fa', 'Monospace', '-fs', '12', '-fg', 'white', '-bg', 'black', '-geometry', '70x18', '-e', comm]
+            subprocess.Popen(command)
+        except:
+            pass
