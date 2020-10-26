b0c46f9982c5f9e6b94446c8e3692e9811759828
frank038 <42263206+frank038@users.noreply.github.com>
2020-10-26 08:39:35 +0100

    Update cfg.py

--- qmfm/cfg.py
+++ qmfm/cfg.py
@@ -25,14 +25,10 @@ ITEM_SPACE = 25
 FONT_SIZE = 10
 # show delete context menu entry that bypass the trashcan: 0 no - 1 yes
 USE_DELETE = 1
-# use the Paste and Merge action - should be safe: 0 no - 1 yes - 2 yes and hide the Paste action
-USE_PM = 2 
 # load the trash module: 0 no - 1 yes
 USE_TRASH = 0
 # load the media module: 0 no - 1 yes
 USE_MEDIA = 0
-# show the history: 0 no - 1 yes
-SHOW_HISTORY = 1
 # Paste and Merge, how to backup the new files: 0 add progressive number
 # in the form _(#) - 1 add date and time (without checking eventually
 # existing file at destination with same date and time suffix) 
